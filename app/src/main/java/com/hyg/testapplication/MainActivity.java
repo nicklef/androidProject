69b6a8ea8485cce9d3e8f5bb6f27d9d4b51c6822
Nick <nleffers@icloud.com>
2021-09-11 17:37:45 -0400

    First Commit

--- app/src/main/java/com/hyg/testapplication/MainActivity.java
+++ app/src/main/java/com/hyg/testapplication/MainActivity.java
@@ -3,12 +3,28 @@ package com.hyg.testapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mainTV;
+    Button workBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainTV = findViewById(R.id.textView);
+        workBtn = findViewById(R.id.button);
+
+        workBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mainTV.setText("It Worked");
+            }
+        });
     }
 }
